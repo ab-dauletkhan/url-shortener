b99bc5f35a1c412fd1b858ab5728383b26bf90d8
ab-dauletkhan <ab.dauletkhan@gmail.com>
2024-07-09 13:32:58 +0500

    added storage

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,56 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+
+	"github.com/ab-dauletkhan/url-shortener/internal/config"
+	"github.com/ab-dauletkhan/url-shortener/internal/lib/logger/sl"
+	"github.com/ab-dauletkhan/url-shortener/internal/storage/sqlite"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
 func main() {
-	// TODO: init config - cleanenv
+	cfg := config.MustLoad()
 
-	// TODO: init logger - slog
+	log := setupLogger(cfg.Env)
 
-	// TODO: init db - sqlite
+	log.Info("program started", slog.String("env", cfg.Env))
+	log.Debug("debug logs are enabled")
 
-	// TODO: init router - chi
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,54 @@
 package config
 
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
-	Env string `yaml:"env" env-default:"local"`
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	TimeOut     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeOut time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	err := godotenv.Load("local.env")
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Error getting working directory:", err)
+	}
+
+	configPath = filepath.Join(wd, configPath)
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("Config file does not exist:", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatal("Cannot read config:", err)
+	}
+
+	return &cfg
 }
